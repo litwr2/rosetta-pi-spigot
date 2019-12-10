4976d851f088fedaefaf9a591985b4c5eacb5dbc
micro <litwr@yandex.ru>
2019-12-11 01:11:43 +0300

    unix-v7/pi.c - a better getfree()

--- pdp-11/unix-v7/pi.c
+++ pdp-11/unix-v7/pi.c
@@ -6,46 +6,34 @@ char *ra;
 extern char ver;
 unsigned digits, max, N;
 unsigned getfree() {
-    unsigned new, min = 0, max = 65000;
-    char *p;
-    while (max - min > 2) {
-        new = max/2 + min/2;
-        if (p = malloc(new)) {
-            free(p);
-            min = new;
-        } else
-            max = new;
-    }
-    return min + 1;
+    unsigned max = 0, step = 256;
+    do {
+        while (sbrk(step) != -1)
+            max += step;
+        step /= 2;
+    } while (step > 8);
+    return max;
 }
 main() {
    printf("Number pi calculator v%s\n", &ver);
-l1:
+   ra = sbrk(0);
    max = (getfree()/7)&~3;
    do {
-      char s[8];
+      char s[8], *p;
       do {
          printf("Enter number of digits (up to %d): ", max);
-         ra = gets(s);
+         p = gets(s);
       }
-      while (!ra || strlen(ra) > 4);
+      while (!p || strlen(s) > 4);
       digits = atoi(s);
-   }
-   while (digits > max || digits == 0);
+   } while (digits > max || digits == 0);
    if ((digits & 3) != 0) {
       digits = (digits + 3) & 0xfffc;
       printf("%u digits will be printed\n", digits);
    }
    N = digits/2*7;
-   if (ra = (char*)malloc(digits*7)) {
-      ftime(&tps);
-      pistart();
-      ftime(&tpf);
-      printf(" %.3f\n", ((tpf.time - tps.time)*1000. + tpf.millitm - tps.millitm)/1000.);
-      free(ra);
-   }
-   else {
-      fprintf(stderr, "cannot allocate memory, use less digits\n");
-      goto l1;
-   }
+   ftime(&tps);
+   pistart();
+   ftime(&tpf);
+   printf(" %.3f\n", ((tpf.time - tps.time)*1000. + tpf.millitm - tps.millitm)/1000.);
 }
